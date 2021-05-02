9ecf5fda14318352ae90190682835d7551678afe
rusefillc <sdfsdfqsf2334234234>
2021-05-02 17:48:21 -0400

    Fab time! Let's see how it goes :)

--- copy_121vag.sh
+++ copy_121vag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ../hellen-one
-python2.7 ./bin/copy_from_Kicad.py "frames" ".." "kicad6/gerber" "121vag" "a"
+python2.7 ./bin/copy_from_Kicad.py "frames" ".." "kicad6/gerber" "121vag" "b"
 
 echo "Done!"
--- create_hellen121vag.sh
+++ create_hellen121vag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ../hellen-one
-sh ./bin/create_board.sh "../hellen121vag" "121vag" "a" "bom_replace_121vag-a.csv"
+sh ./bin/create_board.sh "../hellen121vag" "121vag" "b" "bom_replace_121vag-b.csv"
