5f33ab4d3ce66c8ecc7eacf1dffceddbd85ceab4
Vladimir Vinogradov <qwerty-off@mail.ru>
2021-05-18 20:30:39 +0300

    New vrml

---
+++ boards/hellen121vag-b/kicad6/export_gerbers.py
@@ -0,0 +1,18 @@
+#
+# https://electronics.stackexchange.com/questions/390135/command-line-interface-for-kicad
+#
+
+# Load board and initialize plot controller
+board = pcbnew.LoadBoard("hellen121vag.kicad_pcb")
+pc = pcbnew.PLOT_CONTROLLER(board)
+po = pc.GetPlotOptions()
+po.SetPlotFrameRef(False)
+
+# Set current layer
+pc.SetLayer(pcbnew.F_Cu)
+
+# Plot single layer to file
+pc.OpenPlotfile("front_copper", pcbnew.PLOT_FORMAT_GERBER, "front_copper")
+print("Plotting to " + pc.GetPlotFileName())
+pc.PlotLayer()
+pc.ClosePlot()
