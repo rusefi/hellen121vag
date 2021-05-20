18177587d6b9823f641ee704b7b99333c82af0b2
Vladimir Vinogradov <qwerty-off@mail.ru>
2021-05-20 20:33:17 +0300

    new folder structure

--- boards/hellen121vag-b/kicad6/step_0_export_gerbers.sh
+++
@@ -1,5 +0,0 @@
-
-# todo: generate everything to gerbersAutomated according to https://github.com/andreika-git/hellen-one/wiki/kicad-export-frame
-# until content matches with manually generated
-
-C:\Program Files\KiCad\5.99\bin export_python.py
---
+++ step2_copy_hellen121vag.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd hellen-one
+python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "121vag" "b"
+
+echo "Done!"
