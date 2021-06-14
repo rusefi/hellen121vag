e488ff9bb826dc3f52a85c0ade7fc5bcfa7cd51e
rusefillc <sdfsdfqsf2334234234>
2021-06-14 17:35:09 -0400

    Trying to fab rev B fix #46

--- step2_copy_hellen121vag.sh
+++ step2_copy_hellen121vag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd hellen-one
-python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "121vag" "b"
+python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "121vag" "c"
 
 echo "Done!"
--- step3_create_hellen121vag.sh
+++ step3_create_hellen121vag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd hellen-one
-sh bin/create_board.sh ".." "121vag" "b" "bom_replace_121vag-b.csv"
+sh bin/create_board.sh ".." "121vag" "c" "bom_replace_121vag-c.csv"
