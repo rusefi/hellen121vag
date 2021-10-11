97e07f71dbc08724707f22460f17d2587f140c37
Vladimir Vinogradov <qwerty-off@mail.ru>
2021-10-12 01:30:27 +0300

    rev. c fab time?

--- step2_copy_hellen121vag.sh
+++ step2_copy_hellen121vag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd hellen-one
-python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "121vag" "c"
+python2.7 ./bin/copy_from_Kicad.py "frames:hellen" "../.." "../../gerber" "121vag" "c"
 
 echo "Done!"
--- step3_create_hellen121vag.sh
+++ step3_create_hellen121vag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd hellen-one
-sh bin/create_board.sh ".." "121vag" "c" "bom_replace_121vag-c.csv"
+sh bin/create_board_with_prefix.sh "hellen" ".." "121vag" "c" "bom_replace_121vag-c.csv" "80,0"
