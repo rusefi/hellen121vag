3371e20d2d2056cdad2bf10f07ce60a190f4cac2
rusefillc <sdfsdfqsf2334234234>
2021-11-07 00:38:59 -0400

    auto-pull submodules

--- step3_create_hellen121vag.sh
+++ step3_create_hellen121vag.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ ! -f "hellen-one/readme.md" ]; then
+    echo "No submodule? Trying to pull"
+    git submodule update --init
+fi
+
 cd hellen-one
 sh bin/create_board_with_prefix.sh "hellen" ".." "121vag" "c" "bom_replace_121vag-c.csv" "80,0"
